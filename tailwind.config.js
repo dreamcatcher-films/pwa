15f825f2ba3f7c154d01d9c55ae5f0cefa5af1ec
dreamcatcher-films <s.kossakowski.uk@gmail.com>
2025-07-17 15:39:48 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -10,10 +12,20 @@ export default {
         'modal-in': {
           '0%': { transform: 'scale(0.9)', opacity: '0' },
           '100%': { transform: 'scale(1)', opacity: '1' },
+        },
+        'slide-in': {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        'slide-out': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
         }
       },
       animation: {
         'modal-in': 'modal-in 0.2s ease-out forwards',
+        'slide-in': 'slide-in 0.3s ease-out forwards',
+        'slide-out': 'slide-out 0.3s ease-out forwards',
       }
     },
   },
