8e61aff7f02524e12375ec2c639bd2df02309e1b
Sebastian <s.kossakowski.uk@gmail.com>
2025-07-20 14:02:48 +0100

    Update tailwind.config.js [deploy]

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,12 +29,17 @@ export default {
           '0%': { transform: 'translateY(100%)', opacity: '0' },
           '100%': { transform: 'translateY(0)', opacity: '1' },
         },
+        'scroll-filmstrip': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        }
       },
       animation: {
         'modal-in': 'modal-in 0.2s ease-out forwards',
         'slide-in': 'slide-in 0.3s ease-out forwards',
         'slide-out': 'slide-out 0.3s ease-out forwards',
         'slide-in-bottom': 'slide-in-bottom 0.3s ease-out forwards',
+        'scroll-filmstrip': 'scroll-filmstrip 60s linear infinite',
       }
     },
   },
