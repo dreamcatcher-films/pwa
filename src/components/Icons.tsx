0a65098c9369b727bfade331a9b81b0bb78a42b4
Sebastian <s.kossakowski.uk@gmail.com>
2025-07-21 12:44:38 +0100

    Update Icons.tsx

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -50,6 +50,26 @@ export const LoadingSpinner: React.FC<{className?: string}> = ({ className = 'w-
     </svg>
 );
 
+export const EngagementRingSpinner: React.FC<{className?: string}> = ({ className = 'w-16 h-16 text-slate-700' }) => (
+    <svg className={className} viewBox="-18 -18 100 100" xmlns="http://www.w3.org/2000/svg">
+      <g fill="none" strokeWidth="4" stroke="currentColor">
+        <path
+          className="animate-draw-ring"
+          strokeDasharray="157"
+          strokeDashoffset="157"
+          d="M 32 5 C 46.911688 5 59 17.088312 59 32 C 59 46.911688 46.911688 59 32 59 C 17.088312 59 5 46.911688 5 32 C 5 17.088312 17.088312 5 32 5 Z"
+        />
+        <path
+          className="opacity-0"
+          style={{ animation: 'pulse 1.5s ease-in-out infinite' }}
+          fill="currentColor"
+          d="M28,2 L36,2 L32,8 L28,2 Z"
+        />
+      </g>
+    </svg>
+);
+
+
 export const ChatBubbleBottomCenterTextIcon: React.FC<{className?: string}> = ({ className = 'w-6 h-6' }) => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
@@ -88,7 +108,7 @@ export const PhotoIcon: React.FC<{className?: string}> = ({ className = 'w-6 h-6
 
 export const EnvelopeIcon: React.FC<{className?: string}> = ({ className = 'w-6 h-6' }) => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25-2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
     </svg>
 );
 
