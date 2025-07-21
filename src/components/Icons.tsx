a82a167713e58376a25ddf02e62932df9a3b0914
Sebastian <s.kossakowski.uk@gmail.com>
2025-07-21 11:07:45 +0100

    Update Icons.tsx [deploy]

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -268,3 +268,9 @@ export const CameraIcon: React.FC<{className?: string}> = ({ className = 'w-6 h-
         <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
     </svg>
 );
+
+export const ChevronDownIcon: React.FC<{className?: string}> = ({ className = 'w-6 h-6' }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+    </svg>
+);
