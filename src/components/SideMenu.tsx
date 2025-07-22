5d42737522478383f1e996851252d12b820fb3e5
Sebastian <s.kossakowski.uk@gmail.com>
2025-07-22 12:47:51 +0100

    Update SideMenu.tsx [deploy]

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -25,14 +25,14 @@ const SideMenu: React.FC<SideMenuProps> = ({ isOpen, onClose }) => {
         <>
             {/* Overlay */}
             <div 
-                className={`fixed inset-0 bg-black z-50 transition-opacity duration-300 ${isOpen ? 'bg-opacity-50' : 'bg-opacity-0 pointer-events-none'}`}
+                className={`fixed inset-0 bg-black z-120 transition-opacity duration-300 ${isOpen ? 'bg-opacity-50' : 'bg-opacity-0 pointer-events-none'}`}
                 onClick={onClose}
                 aria-hidden="true"
             ></div>
 
             {/* Side Menu */}
             <div 
-                className={`fixed top-0 left-0 h-full bg-[#0F3E34] w-72 shadow-xl z-60 flex flex-col transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
+                className={`fixed top-0 left-0 h-full bg-[#0F3E34] w-72 shadow-xl z-120 flex flex-col transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
                 role="dialog"
                 aria-modal="true"
             >
