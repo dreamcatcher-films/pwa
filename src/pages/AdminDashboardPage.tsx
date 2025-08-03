34a17220c43a9dfb77da93ef749d7d2c8df81e0b
Sebastian <s.kossakowski.uk@gmail.com>
2025-08-03 21:41:37 +0200

    Update AdminDashboardPage.tsx [deploy]

--- src/pages/AdminDashboardPage.tsx
+++ src/pages/AdminDashboardPage.tsx
@@ -97,7 +97,7 @@ const AdminDashboardPage: React.FC = () => {
             {/* Overlay for mobile */}
             {isSidebarOpen && (
                 <div 
-                    className="fixed inset-0 bg-black bg-opacity-50 z-20 md:hidden"
+                    className="fixed inset-0 bg-black bg-opacity-50 z-21 md:hidden"
                     onClick={() => setIsSidebarOpen(false)}
                 ></div>
             )}
