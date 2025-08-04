85e2e89287db9e21cdb5d63bbac7248cd8da3110
Sebastian <s.kossakowski.uk@gmail.com>
2025-08-04 12:38:52 +0200

    Update Icons.tsx

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -136,7 +136,7 @@ export const LockClosedIcon: React.FC<{className?: string}> = ({ className = 'w-
 
 export const UserGroupIcon: React.FC<{className?: string}> = ({ className = 'w-6 h-6' }) => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-      <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m-7.5-2.962A3.75 3.75 0 0112 15v-2.25A3.75 3.75 0 0115.75 9h.008v.008h-.008A3.75 3.75 0 0112 12.75v-2.25A3.75 3.75 0 019.75 7.5h.008v.008h-.008A3.75 3.75 0 016 11.25v2.25A3.75 3.75 0 018.25 18-3.75 3.75 0 016 15.75v-2.25A3.75 3.75 0 019.75 9.75" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-2.458a9.337 9.337 0 00-4.12-2.458A9.38 9.38 0 0015 14.128v5zM12 15.375a3.375 3.375 0 01-3.375-3.375V8.625a3.375 3.375 0 013.375-3.375h.008a3.375 3.375 0 013.375 3.375v3.375a3.375 3.375 0 01-3.375 3.375H12zM9 14.128a9.38 9.38 0 01-2.625.372 9.337 9.337 0 01-4.121-2.458a9.337 9.337 0 014.12-2.458A9.38 9.38 0 019 19.128v-5z" />
     </svg>
 );
 
